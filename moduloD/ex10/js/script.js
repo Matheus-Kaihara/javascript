d1a8efa7c8eb4d829f6a9119fc7cffe259194db0
Matheus Ryoske Saito Kaihara <134160673+Matheus-Kaihara@users.noreply.github.com>
2024-02-02 15:43:15 -0300

    Finalizado o exercício 10 sobre Condições

---
+++ moduloD/ex10/js/script.js
@@ -0,0 +1,16 @@
+var velocidade = 60.5
+var carro = 'Azul'
+
+// CONDIÇÃO SIMPLES 
+if(carro == 'Azul'){
+    console.log(`O seu carro é ${carro}. \n`)
+}
+
+
+// CONDIÇÃO COMPOSTA
+if(velocidade > 60){
+    console.log('Você ultrapassou o limite de velocidade. Multa!')
+}
+else{
+    console.log(`A velocidade é de ${velocidade} km/h.`)
+}
