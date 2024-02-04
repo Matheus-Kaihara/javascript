a918ff6571b457f3b9c7c56373293cd18efaf8cc
Matheus Ryoske Saito Kaihara <134160673+Matheus-Kaihara@users.noreply.github.com>
2024-02-04 00:07:13 -0300

    Finalizado o exercício 15 sobre For

---
+++ moduloE/ex15/js/script.js
@@ -0,0 +1,17 @@
+// --- FOR ---
+// Comando de repetição com variável de controle 
+// 
+//  1. Inicialização
+//  2. Teste lógico 
+//  3. Incremento da variável
+
+
+
+console.log('--- Tabuada do 5 ---')
+
+for(var contador = 1; contador <= 10; contador++){
+    var tabuada = contador * 5
+
+    console.log(`${contador} x 5 = ${tabuada}`)
+}
+
