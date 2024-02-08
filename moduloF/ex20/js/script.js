53bcec69e79de74f6babc68670f0ee1045a28f29
Matheus Ryoske Saito Kaihara <134160673+Matheus-Kaihara@users.noreply.github.com>
2024-02-08 00:38:58 -0300

    Finalizado o exercício final de Contagem de Números

--- moduloF/ex20/js/script.js
+++ moduloF/ex20/js/script.js
@@ -3,7 +3,7 @@ let display = document.querySelector('select#id_display')
 let txtResultado = document.querySelector('div#id_txtResultado')
 let array = []
 
-function isRange(n){
+function inRange(n){
     if(Number(n) >= 1 && Number(n) <= 100){
         return true;
     } else{
@@ -21,15 +21,45 @@ function inArray(n, l){
 }
 
 function addNumber(){
-    if(isRange(num.value) && !inArray(num.value, array)){
+    if(inRange(number.value) && !inArray(number.value, array)){
         array.push(Number(number.value))
         let item = document.createElement('option')
         item.text = `Valor ${number.value} adicionado`
         display.appendChild(item)
+        txtResultado.innerHTML = ''
     }else{
         alert('[ALERTA] Valor inválido ou já está na lista')
     }
 
     number.value = ''
     number.focus() 
+}
+
+function endNumber(){
+    if(array.length == 0){
+        window.alert('[ALERTA] Insira valores antes de finalizar')
+    } else{
+        let elementos = array.length
+        let maior = array[0]
+        let menor = array[0]
+        let soma = 0
+
+        for(let i in array){
+            soma += array[i]
+            if(array[i] > maior){
+                maior = array[i]
+            }
+            else if(array[i] < menor){
+                menor = array[i]
+            }
+        }
+
+
+        txtResultado.innerHTML = ''
+        txtResultado.innerHTML += `<p>Ao todo, temos ${elementos} números cadastrados.</p>`
+        txtResultado.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        txtResultado.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        txtResultado.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        txtResultado.innerHTML += `<p>A média dos valores digitados é ${(soma/elementos).toFixed(2)}</p>`
+    }
 }
